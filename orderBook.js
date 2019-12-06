4430db6bd7112fc9bb36b25c43d674250f47e35d
Upasana Tanvir <upasana@Upasanas-MacBook-Pro.local>
2019-12-06 12:58:58 -0500

    All test passed

--- orderBook.js
+++ orderBook.js
@@ -1,5 +1,59 @@
+/* eslint-disable indent */
 function reconcileOrder(existingBook, incomingOrder) {
-  throw new Error('Not Implemented Yet')
+
+    var updatedBook = []
+
+
+    if (existingBook.length == 0) {
+
+        updatedBook.push(incomingOrder)
+        return updatedBook
+    }
+    else if (existingBook[0]['type'] == 'sell' && incomingOrder['type'] == 'sell') {
+
+
+        updatedBook.push(existingBook[0], incomingOrder)
+
+    }
+    else if (existingBook[0]['type'] == 'buy' && incomingOrder['type'] == 'sell') {
+        if (existingBook[0]['quantity'] == incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
+
+            return updatedBook
+
+        }
+        else if (existingBook[0]['quantity'] != incomingOrder['quantity'] && existingBook[0]['price'] != incomingOrder['price']) {
+
+            updatedBook.push(existingBook[0], incomingOrder)
+
+        }
+        else if (existingBook[0]['quantity'] == incomingOrder['quantity'] && existingBook[0]['price'] != incomingOrder['price']) {
+            if (existingBook[0]['price'] < incomingOrder['price']) {
+
+                updatedBook.push(existingBook[0], incomingOrder)
+
+            }
+            else if (existingBook[0]['price'] > incomingOrder['price']) {
+
+                return updatedBook
+            }
+
+        }
+        else if (existingBook[0]['quantity'] > incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
+
+            updatedBook.push(existingBook[0])
+            updatedBook[0]['quantity'] = existingBook[0]['quantity'] - incomingOrder['quantity']
+
+        }
+        else if (existingBook[0]['quantity'] < incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
+
+            updatedBook.push(incomingOrder)
+            updatedBook[0]['quantity'] = incomingOrder['quantity'] - existingBook[0]['quantity']
+
+        }
+    }
+
+    return updatedBook
+
 }
 
 module.exports = reconcileOrder
