68ce2a8f24d36f581ce80b1349ad0f7ff9e3a5c1
Upasana Tanvir <upasana@Upasanas-MacBook-Pro.local>
2019-12-09 09:55:32 -0500

    Version2 with functions added

--- orderBook.js
+++ orderBook.js
@@ -1,59 +1,64 @@
 /* eslint-disable indent */
-function reconcileOrder(existingBook, incomingOrder) {
+function matchingOrder(existingBook, incomingOrder) {
+    let matchingOrder = existingBook.filter((item) => {
+        return item.type !== incomingOrder.type &&
+            item.price === incomingOrder.price
+    })
+    return matchingOrder
+}
 
-    var updatedBook = []
+function updatesMatchingOrder(existingBook, incomingOrder) {
+    let updatedBook = []
+    const matchingIndex = existingBook.findIndex((item) => {
+        return incomingOrder.type !== item.type &&
+            incomingOrder.quantity === item.quantity &&
+            incomingOrder.price === item.price
+    })
+    const partialMatchingIndex = existingBook.findIndex((item) => {
+        return incomingOrder.type !== item.type &&
+            incomingOrder.price === item.price
+    })
 
-
-    if (existingBook.length == 0) {
-
-        updatedBook.push(incomingOrder)
-        return updatedBook
-    }
-    else if (existingBook[0]['type'] == 'sell' && incomingOrder['type'] == 'sell') {
-
-
-        updatedBook.push(existingBook[0], incomingOrder)
-
-    }
-    else if (existingBook[0]['type'] == 'buy' && incomingOrder['type'] == 'sell') {
-        if (existingBook[0]['quantity'] == incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
-
-            return updatedBook
-
-        }
-        else if (existingBook[0]['quantity'] != incomingOrder['quantity'] && existingBook[0]['price'] != incomingOrder['price']) {
-
-            updatedBook.push(existingBook[0], incomingOrder)
-
-        }
-        else if (existingBook[0]['quantity'] == incomingOrder['quantity'] && existingBook[0]['price'] != incomingOrder['price']) {
-            if (existingBook[0]['price'] < incomingOrder['price']) {
-
-                updatedBook.push(existingBook[0], incomingOrder)
-
-            }
-            else if (existingBook[0]['price'] > incomingOrder['price']) {
-
-                return updatedBook
-            }
-
-        }
-        else if (existingBook[0]['quantity'] > incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
-
-            updatedBook.push(existingBook[0])
-            updatedBook[0]['quantity'] = existingBook[0]['quantity'] - incomingOrder['quantity']
-
-        }
-        else if (existingBook[0]['quantity'] < incomingOrder['quantity'] && existingBook[0]['price'] == incomingOrder['price']) {
-
-            updatedBook.push(incomingOrder)
-            updatedBook[0]['quantity'] = incomingOrder['quantity'] - existingBook[0]['quantity']
-
+    if (matchingIndex > -1) {
+        updatedBook = removesMatchingOrder(matchingIndex, existingBook)
+    } else {
+        updatedBook = existingBook
+        let existingOrder = updatedBook[partialMatchingIndex]
+        if (existingOrder.quantity > incomingOrder.quantity) {
+            updatedBook[partialMatchingIndex].quantity = existingOrder.quantity - incomingOrder.quantity
+        } else {
+            updatedBook[partialMatchingIndex].quantity = incomingOrder.quantity - existingOrder.quantity
+            updatedBook[partialMatchingIndex].type = incomingOrder.type
         }
     }
-
     return updatedBook
+}
 
+function removesMatchingOrder(matchingIndex, existingBook) {
+    let updatedBook = existingBook
+    updatedBook.splice(matchingIndex, 1)
+    return updatedBook
+}
+
+function getUpdatedBook(existingBook, incomingOrder) {
+    let updatedBook = []
+    let matchingOrders = matchingOrder(existingBook, incomingOrder)
+    if (matchingOrders.length) {
+        updatedBook = updatesMatchingOrder(existingBook, incomingOrder)
+    } else {
+        updatedBook = existingBook.concat(incomingOrder)
+    }
+    return updatedBook
+}
+
+function reconcileOrder(existingBook, incomingOrder) {
+    let updatedBook = []
+    if (!existingBook.length) {
+        updatedBook = existingBook.concat(incomingOrder)
+    } else {
+        updatedBook = getUpdatedBook(existingBook, incomingOrder)
+    }
+    return updatedBook
 }
 
 module.exports = reconcileOrder
