37faf47035600fa19d4205937b2429f80b89c3ff
Upasana Tanvir <upasana@Upasanas-MacBook-Pro.local>
2019-12-09 09:58:47 -0500

    version 2 with functions

--- orderBook.js
+++ orderBook.js
@@ -1,4 +1,4 @@
-/* eslint-disable indent */
+
 function matchingOrder(existingBook, incomingOrder) {
     let matchingOrder = existingBook.filter((item) => {
         return item.type !== incomingOrder.type &&
